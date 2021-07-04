62ee569dc5c56d0ccbea584142aff35a45e869fa
Andre Alves Garzia <andre@andregarzia.com>
2021-07-04 14:24:42 +0100

    wrong getting started link

--- src/press/Press.svelte
+++ src/press/Press.svelte
@@ -173,7 +173,7 @@
           <p class="text-xl">{$_("no_book")}</p>
           <p class="text-light">{$_("drag-and-drop-to-start")}</p>
           <div class="mt-6">
-            <a class="btn btn-blue" href="/documentation/getting-started">
+            <a class="btn btn-blue" href="/documentation/en/getting-started">
               {@html $_("learn-more-long")}
             </a>
           </div>
