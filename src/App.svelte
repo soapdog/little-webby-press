b1773f5e372093fe4309eacb36ee500f6e755819
Andre Alves Garzia <andre@andregarzia.com>
2021-07-04 20:38:44 +0100

    [[ nav ]] basic router.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
-	import Nav from "./common/Nav.svelte";
-	import Press from "./press/Press.svelte";
+	import Nav from "./common/Nav.svelte"
+	import Press from "./press/Press.svelte"
+  import {currentView} from "./common/viewManager.js"
+
+  const views = {
+    press: Press
+  }
 </script>
 
 <div class="w-1/3 ml-auto mr-auto h-12">
 	<Nav />
-	<Press />
+	<svelte:component this={views[$currentView]}  />
 </div>
