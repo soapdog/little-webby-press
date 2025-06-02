0354eea6ac490abab40dd5babbc671b28b7654d7
Andre Alves Garzia <andre@andregarzia.com>
2025-06-02 14:28:25 +0100

    changing default route.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import m from "mithril"
-import Press from "./press.js"
-import Documentation from "./documentation.js"
-import About from "./about.js"
+import m from "mithril";
+import Press from "./press.js";
+import Documentation from "./documentation.js";
+import About from "./about.js";
 
-const root = document.body
+const root = document.body;
 
-m.route(root, "/about", {
-	"/press": Press,
-	"/documentation": Documentation,
-	"/about": About
-})
+m.route(root, "/press", {
+  "/press": Press,
+  "/documentation": Documentation,
+  "/about": About,
+});
